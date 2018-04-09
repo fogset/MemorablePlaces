6d07458a6481971228b6932f33e1bc541cc2d411
Tianhao <zhangth1990@hotmail.com>
2018-04-08 23:45:41 -0600

    Added google map activity

--- app/src/main/java/com/example/tianhao/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/tianhao/memorableplaces/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tianhao.memorableplaces;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,8 +19,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView = findViewById(R.id.listVIew);
-        ArrayList<String> places = new ArrayList<String>();
+        final ArrayList<String> places = new ArrayList<String>();
         places.add("Add a new place...");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                intent.putExtra("placeNumber", position);
+                startActivity(intent);
+            }
+        });
 
 
     }
