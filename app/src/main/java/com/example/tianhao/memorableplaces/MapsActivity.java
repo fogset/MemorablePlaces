80b4a209406235254a3c09a3ddc4c5facf6713bc
Tianhao <zhangth1990@hotmail.com>
2018-04-08 23:56:52 -0600

    Added new marker

--- app/src/main/java/com/example/tianhao/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/example/tianhao/memorableplaces/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.example.tianhao.memorableplaces;
 
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,9 +15,14 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+    LocationManager locationManager;
+    LocationManager locationListener;
     private GoogleMap mMap;
 
+    public void centerMapOnLocation(Location location, String title){
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.clear();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +34,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        Intent intent = getIntent();
+        if(intent.getIntExtra("placeNumber",0) == 0){
+
+        }
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
